f23db8ac10fa4e4660d6bed9d7e6042ecf1e5625
Túlio <tulio.engcomp@outlook.com>
2019-08-14 18:47:48 -0300

    Adicionando dependencias jpa + map scrupt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+
 plugins {
     kotlin("plugin.jpa") version "1.2.71"
     id("org.springframework.boot") version "2.1.7.RELEASE"
     id("io.spring.dependency-management") version "1.0.7.RELEASE"
     kotlin("jvm") version "1.2.71"
     kotlin("plugin.spring") version "1.2.71"
+    kotlin("kapt") version "1.2.71"
 }
 
 group = "com.homeio"
@@ -25,12 +27,15 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.mapstruct:mapstruct-jdk8:1.3.0.Final")
+    kapt("org.mapstruct:mapstruct-processor:1.3.0.Final")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    runtimeOnly("org.postgresql:postgresql")
+    implementation("com.h2database:h2:1.4.199")
+    implementation("org.flywaydb:flyway-core:5.2.4")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
