7c74bb565b6ffcebf44f417a240cf840aa9b60c2
Túlio <tulio.engcomp@outlook.com>
2019-08-14 09:05:38 -0300

    Load code project

---
+++ src/main/kotlin/com/spring/revisao/RevisaoApplication.kt
@@ -0,0 +1,11 @@
+package com.spring.revisao
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RevisaoApplication
+
+fun main(args: Array<String>) {
+    runApplication<RevisaoApplication>(*args)
+}
