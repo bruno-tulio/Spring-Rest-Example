9928eb9f0df3cae6dc0e2e052ea12556fabd446a
Túlio <tulio.engcomp@outlook.com>
2019-08-15 11:12:54 -0300

    Atualizando resopuce adicionando outro endepoints mas validacao

--- src/main/kotlin/com/homeio/forum/resources/v1/TopicsResources.kt
+++ src/main/kotlin/com/homeio/forum/resources/v1/TopicsResources.kt
@@ -2,29 +2,51 @@ package com.homeio.forum.resources.v1
 
 import com.homeio.forum.dto.TopicoDto
 import com.homeio.forum.mapper.TopicoMapper
-import com.homeio.forum.model.Curso
-import com.homeio.forum.model.StatusTopico
 import com.homeio.forum.model.Topico
-import com.homeio.forum.model.Usuario
 import com.homeio.forum.repository.TopicoRepository
+import com.homeio.forum.repository.excption.EntityNotFoundException
+import com.homeio.forum.repository.filter.TopicoFilter
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDateTime
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.util.UriComponentsBuilder
+import javax.validation.Valid
+
 
 @RestController
-@RequestMapping("${VersionV1Resources.VERSION}/topics")
-class TopicsResources{
-
-
+@RequestMapping("${VersionV1Resources.VERSION}/topicos")
+class TopicsResources {
 
     @Autowired
     private lateinit var topicoRepository: TopicoRepository
 
     @GetMapping
-    fun getTopics(): List<TopicoDto> {
-
-        return topicoRepository.findAll().map {  TopicoMapper.INSTANCE.topicoToTopicoDTO(it) }
+    fun get(topicoFilter: TopicoFilter): ResponseEntity<List<TopicoDto>?> {
+        return ResponseEntity.ok(topicoRepository.findAllFilter(topicoFilter).let { topicos -> topicos?.map { TopicoMapper.INSTANCE.topicoToTopicoDTO(it) } ?: listOf() })
     }
+
+    @GetMapping("/{id}")
+    fun get(@PathVariable id: Long): ResponseEntity<Topico> {
+        return ResponseEntity.ok(topicoRepository.findById(id).orElseThrow {  EntityNotFoundException("Topico não existe") })
+    }
+
+    @PostMapping
+    fun salvar(@RequestBody @Valid topico: Topico, uriComponentsBuilder: UriComponentsBuilder): ResponseEntity<Any> {
+        val topico  = topicoRepository.save(topico)
+        val uri = uriComponentsBuilder.path("topicos/{codigo}").buildAndExpand(topico.id).toUri()
+        return ResponseEntity.created(uri).body(TopicoMapper.INSTANCE.topicoToTopicoDTO(topico))
+
+    }
+
+    @PutMapping("/{id}")
+    fun update(@PathVariable id :Long, @RequestBody @Valid topico: Topico): ResponseEntity<Any> {
+
+        val topicoExistente = topicoRepository.findById(id).orElseThrow {  EntityNotFoundException("Topico não existe") }
+        val topicoUpdate = topico.copy(id = topicoExistente.id)
+
+        topicoRepository.save(topicoUpdate)
+        return ResponseEntity.noContent().build()
+    }
+
+
 }
